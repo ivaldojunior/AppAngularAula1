1699e2a2c23a18ce24e3335ca7ccea1a375e1f4e
Ivaldo <contato@ivaldo.dev>
2021-04-25 12:31:36 -0300

    ...

---
+++ src/app/produtos/produto-detalhe/produto-detalhe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-detalhe',
+  templateUrl: './produto-detalhe.component.html',
+  styles: [
+  ]
+})
+export class ProdutoDetalheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
